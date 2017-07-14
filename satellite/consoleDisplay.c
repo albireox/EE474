6010c6cf06b0ff70722c9644f25ff58836bee4f1
José Sánchez-Gallego <gallegoj@uw.edu>
2017-07-13 19:13:50 -0700

    moved printWindow to consoleDisplay

--- satellite/consoleDisplay.c
+++ satellite/consoleDisplay.c
@@ -1,3 +1,29 @@
 // consoleDisplay.c
 //
 // Conor Sayres and José Sánchez-Gallego
+
+#include <ncurses.h>
+
+
+void printWindow(unsigned int leds){
+
+        WINDOW *win = newwin(10, 55, 1, 1);
+        refresh();
+
+        box(win, '|', '-');
+        mvwprintw(win, 1, 3, "Extraterrestrial Resources, Ltd. Warnings panel.");
+        mvwhline(win, 2, 1, '=', 53);
+        mvwprintw(win, 3, 20, "WARNING CONSOLE");
+        mvwhline(win, 4, 1, '-', 53);
+
+        int ii;
+        static int jj = 0;
+
+        for (ii=1; ii <= 4; ii++) {
+            mvwprintw(win, ii + 4, 2, "LED %d: %d", ii, jj);
+        }
+
+        touchwin(win);
+        wrefresh(win);
+
+}
