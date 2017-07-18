6a75d2406f481a5aa34a1e9717b20eaf3ab9856e
Demo User <debian@beaglebone.(none)>
2017-07-18 04:53:03 +0000

    added setupGPIO.sh

---
+++ satellite/setupGPIO.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 61 > /sys/class/gpio/export
+echo 66 > /sys/class/gpio/export
+echo 67 > /sys/class/gpio/export
+echo 68 > /sys/class/gpio/export
+echo 69 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio61/direction
+echo out > /sys/class/gpio/gpio66/direction
+echo out > /sys/class/gpio/gpio67/direction
+echo out > /sys/class/gpio/gpio68/direction
+echo out > /sys/class/gpio/gpio69/direction
